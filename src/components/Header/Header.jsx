921250981a9707d7c8bf83e9ab8f956a1f3dee65
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-21 12:21:43 -0300

    feat: melhorias no menu do site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,45 +1,68 @@
 // import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { IconCar, IconSearch, IconUser } from '../../assets/Icon';
 import { ButtonMenu } from '../Button/styles';
 
 import { Conteiner, Background } from './styles';
 function Header() {
+  const [activeLink, setActiveLink] = useState('/');
+  const handleLinkClick = (to, type='link') => {
+    setActiveLink(to);
+  };
   return (
     <Background>
       <Conteiner>
-        <Link to='/' className='logo'>
+        <Link to='/' className='logo'
+        onClick={() => { handleLinkClick('/', 'logo') }}>
           Feane
         </Link>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/menu'>Menu</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/table'>Book Table</Link>
-            </li>
-          </ul>
-        </nav>
         <div>
-          <div className="icons">
-            <button>
-              <IconUser className='icon'/>
-            </button>
-            <button>
-              <IconCar className='icon'/>
-            </button>
-            <button>
-              <IconSearch className='icon'/>
-            </button>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'
+                 className={activeLink === '/' ? 'link-active' : ''}
+                 onClick={() => { handleLinkClick('/') }}>
+                  Home
+                 </Link>
+              </li>
+              <li>
+                <Link to='/menu'
+                 className={activeLink === '/menu' ? 'link-active' : ''}
+                 onClick={() => { handleLinkClick('/menu') }}>
+                  Menu
+                 </Link>
+              </li>
+              <li>
+                <Link to='/about'
+                 className={activeLink === '/about' ? 'link-active' : ''}
+                 onClick={() => { handleLinkClick('/about') }}>
+                  Abou
+                 t</Link>
+              </li>
+              <li>
+                <Link to='/table'
+                 className={activeLink === '/table' ? 'link-active' : ''}
+                 onClick={() => { handleLinkClick('/table') }}>
+                  Book Table</Link>
+              </li>
+            </ul>
+          </nav>
+          <div>
+            <div className="icons">
+              <button>
+                <IconUser className='icon'/>
+              </button>
+              <button>
+                <IconCar className='icon'/>
+              </button>
+              <button>
+                <IconSearch className='icon'/>
+              </button>
+            </div>
+            <ButtonMenu>Order Online</ButtonMenu>
           </div>
-          <ButtonMenu>Order Online</ButtonMenu>
         </div>
       </Conteiner>
     </Background>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,11 @@
 import { styled} from "styled-components";
 import { displayFlex, background, icon } from '../styles';
-import colors from '../../helpers/color'
+import colors from '../../helpers/color';
+import {
+    dancingBold,
+    openRegular,
+    bodyHover
+} from "../../helpers/tipografia";
 
 export const Background = styled.div`
     background: url('src/assets/hero-bg.jpg');
@@ -9,30 +14,39 @@ export const Background = styled.div`
     height: 630px;
 `
 export const Conteiner = styled.header`
-        height: 60px;
-        width: 95%;
-        margin: auto;
-    &, ul{
-        ${displayFlex};
-    }
-    .logo{
-        font-weight: bold;
-        font-size: 32px;
-        color: #${colors.white};
-    }
-    nav a{
-        color: ${colors.white};
-    }
-    .link-active{
-        color: ${colors.yellowLight};
-    }
-    .icon{
-        ${icon};
-        margin-right: 15px;
-    }
+    height: 90px;
+    width: 90%;
+    margin: auto;
+    &, 
+    ul,
     div,
     .icons{
         ${displayFlex};
+        justify-content: space-between;
+    }
+
+    .logo{
+        ${dancingBold}
+        font-size: 32px;
+    }
+
+    nav{
+        width: 360px;
+        margin-right: 35px;
+    }
+    nav a{
+        ${openRegular};
+        ${bodyHover}; 
+        text-transform: uppercase;
+    }
+
+    .link-active{
+        color: ${colors.yellowLight};
+    }
+
+    .icon{
+        ${icon};
+        margin-right: 20px;
     }
     
 `
