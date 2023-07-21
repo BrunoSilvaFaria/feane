50a74d945b23481ae06cd66efe6ca8c8e8fd977d
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-21 16:06:42 -0300

    feat: modificações na estrutura

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,55 +3,73 @@ const menu = [
     name: "Delicious Pizza",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f1.png",
     price: "$20",
+    type: ["All", "Pizza"],
   },
   {
     name: "Delicious Burger",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f2.png",
     price: "$15",
+    type: ["All", "Burger"],
   },
   {
     name: "Delicious Pizza",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f3.png",
     price: "$17",
+    type: ["All", "Pizza"],
   },
   {
     name: "Delicious Pasta",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f4.png",
     price: "$18",
+    type: ["All", "Pasta"],
   },
   {
     name: "French Fries",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f5.png",
     price: "$10",
+    type: ["All", "Fries"],
   },
   {
     name: "Delicious Pizza",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f6.png",
     price: "$15",
+    type: ["All", "Pizza"],
   },
   {
     name: "Tasty Burger",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f7.png",
     price: "$12",
+    type: ["All", "Burger"],
   },
   {
     name: "Tasty Burger",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f8.png",
     price: "$14",
+    type: ["All", "Burger"],
   },
   {
     name: "Delicious Pasta",
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    img: "/assets/f9.png",
     price: "$10",
+    type: ["All", "Pasta"],
   },
 ];
 
