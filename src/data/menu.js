36fe2d10edeb71e630fbc16b68243947a896304b
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-21 14:17:57 -0300

    feat: adição de objeto, o qual, será utilizado para criação de componentes

---
+++ src/data/menu.js
@@ -0,0 +1,58 @@
+const menu = [
+  {
+    name: "Delicious Pizza",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$20",
+  },
+  {
+    name: "Delicious Burger",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$15",
+  },
+  {
+    name: "Delicious Pizza",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$17",
+  },
+  {
+    name: "Delicious Pasta",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$18",
+  },
+  {
+    name: "French Fries",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$10",
+  },
+  {
+    name: "Delicious Pizza",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$15",
+  },
+  {
+    name: "Tasty Burger",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$12",
+  },
+  {
+    name: "Tasty Burger",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$14",
+  },
+  {
+    name: "Delicious Pasta",
+    description:
+      "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
+    price: "$10",
+  },
+];
+
+export default menu;
