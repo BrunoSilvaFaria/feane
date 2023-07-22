f3786c5585085c9427eecf27e9c0055bc7302691
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-22 15:25:13 -0300

    feat: adição de id

--- src/data/menu.js
+++ src/data/menu.js
@@ -4,6 +4,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f1.png",
+    id: 1,
     price: "$20",
     type: ["All", "Pizza"],
   },
@@ -12,6 +13,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f2.png",
+    id: 2,
     price: "$15",
     type: ["All", "Burger"],
   },
@@ -20,6 +22,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f3.png",
+    id: 3,
     price: "$17",
     type: ["All", "Pizza"],
   },
@@ -28,6 +31,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f4.png",
+    id: 4,
     price: "$18",
     type: ["All", "Pasta"],
   },
@@ -36,6 +40,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f5.png",
+    id: 5,
     price: "$10",
     type: ["All", "Fries"],
   },
@@ -44,6 +49,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f6.png",
+    id: 6,
     price: "$15",
     type: ["All", "Pizza"],
   },
@@ -52,6 +58,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f7.png",
+    id: 7,
     price: "$12",
     type: ["All", "Burger"],
   },
@@ -60,6 +67,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f8.png",
+    id: 8,
     price: "$14",
     type: ["All", "Burger"],
   },
@@ -68,6 +76,7 @@ const menu = [
     description:
       "Veniam debitis quaerat officiis quasi cupiditate quo, quisquam velit, magnam voluptatem repellendus sed eaque",
     img: "/assets/f9.png",
+    id: 9,
     price: "$10",
     type: ["All", "Pasta"],
   },
