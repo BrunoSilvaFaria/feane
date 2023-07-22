0d9808e3db6c090b99fee0ce8a2fd5724ea9a116
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-22 11:26:02 -0300

    feat: adição de novos icones do footer

--- src/helpers/Icon.js
+++ src/helpers/Icon.js
@@ -1,10 +1,27 @@
 import {
-    FaShoppingCart,
-    FaSearch,
-    FaUser
-} from 'react-icons/fa';
+  FaEnvelope,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPhone,
+  FaPinterest,
+  FaSearch,
+  FaShoppingCart,
+  FaTwitter,
+  FaUser
+} from "react-icons/fa";
+
+import { MdLocationOn } from "react-icons/md";
 
 export const IconCar = FaShoppingCart;
+export const IconEnvelope =  FaEnvelope;
+export const IconFacebook = FaFacebookF;
+export const IconInstagram = FaInstagram;
+export const IconLinkedin = FaLinkedinIn;
+export const IconLocation = MdLocationOn;
+export const IconPhone =  FaPhone;
+export const IconPinterest = FaPinterest;
 export const IconSearch = FaSearch;
+export const IconTwitter = FaTwitter;
 export const IconUser = FaUser;
 
