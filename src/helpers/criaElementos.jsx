eda6b2b54014c6f74d98d63545f2062691c286d6
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-22 15:25:28 -0300

    feat: adição de motion, para animação

--- src/helpers/criaElementos.jsx
+++ src/helpers/criaElementos.jsx
@@ -1,6 +1,20 @@
-// Corrija a exportação do componente, use somente a exportação nomeada
+import { motion } from "framer-motion";
+
+// Assumindo que você tem um componente "Componente" criado e importado corretamente.
+
 export function criaElementos(arquivo, Componente) {
   return arquivo.map((item) => {
-    return <Componente item={item} />;
+    return (
+      <motion.div
+        key={item.id}
+        layout
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 0.5, 0.8, 1] }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+      >
+        <Componente item={item} />
+      </motion.div>
+    );
   });
 }
