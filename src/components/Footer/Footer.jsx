9184d5275f3bfd15ed2b8937a04c3783e4becb27
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-22 15:45:49 -0300

    feat: adição de estilização ao footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,69 +13,66 @@ import {
 function Footer() {
   return (
     <Conteiner>
-      <div>
-        <h3>Contact Us </h3>
-        <span>
-          <IconLocation />
-          <a href="#">Location</a>
-        </span>
-
-        <span>
-          <IconPhone />
-          <a href="tel=+01 1234567890">Call +01 1234567890</a>
-        </span>
-
-        <span>
-          <IconEnvelope />
-          <a href="mailto:demo@gmail.com">demo@gmail.com</a>
-        </span>
+      <div className="conteiners">
+        <div className="conteiner">
+          <h3>Contact Us </h3>
+          <span>
+            <IconLocation />
+            <a href="#">Location</a>
+          </span>
+          <span>
+            <IconPhone />
+            <a href="tel=+01 1234567890">Call +01 1234567890</a>
+          </span>
+          <span>
+            <IconEnvelope />
+            <a href="mailto:demo@gmail.com">demo@gmail.com</a>
+          </span>
+        </div>
+        <div className="conteiner">
+          <h2>Feane</h2>
+          <p>
+            Necessary,making this the first true generator on the Internet. It
+            uses a dictionary of over 200 Latin words, combined with
+          </p>
+          <nav>
+            <ul className='icons-social'>
+              <li>
+                <Link>
+                  <IconFacebook />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <IconTwitter />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <IconLinkedin />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <IconInstagram />
+                </Link>
+              </li>
+              <li>
+                <Link>
+                  <IconPinterest />
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="conteiner">
+          <h3>Opening Hours</h3>
+          <p>Everyday</p>
+          <p>10.00 Am -10.00 Pm</p>
+        </div>
       </div>
 
-      <div>
-        <h2>Feane</h2>
-        <p>
-          Necessary,making this the first true generator on the Internet. It
-          uses a dictionary of over 200 Latin words, combined with
-        </p>
-
-        <nav>
-          <ul>
-            <li>
-              <Link>
-                <IconFacebook />
-              </Link>
-            </li>
-            <li>
-              <Link>
-                <IconTwitter />
-              </Link>
-            </li>
-            <li>
-              <Link>
-                <IconLinkedin />
-              </Link>
-            </li>
-            <li>
-              <Link>
-                <IconInstagram />
-              </Link>
-            </li>
-            <li>
-              <Link>
-                <IconPinterest />
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-
-      <div>
-        <h3>Opening Hours</h3>
-        <p>Everyday</p>
-        <p>10.00 Am -10.00 Pm</p>
-      </div>
-
-      <div>
+      <div className="conteiner">
         <p>© 2023 All Rights Reserved By Free Html Templates</p>
         <p>© Distributed By ThemeWagon</p>
       </div>
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,6 +1,44 @@
 import { styled} from "styled-components";
-
+import { displayFlex } from '../styles';
+import { openRegular, dancingBold, dancingSemi } from "../../helpers/tipografia";
 
 export const Conteiner = styled.header`
-    background-color: red;
-`
+  background-color: #222831;
+  ${openRegular};
+  font-size: 1em;
+  line-height: 24px;
+  text-align: center;
+  .conteiners {
+    width: 90%;
+    max-width: 960px;
+    margin: auto;
+  }
+  .conteiners,
+  .icons-social {
+    ${displayFlex};
+    align-items: flex-start;
+  }
+
+  .conteiner {
+    h3,
+    h2 {
+      margin-bottom: 25px;
+    }
+    h2 {
+      ${dancingBold};
+      font-size: 38px;
+      line-height: 38px;
+    }
+    h3 {
+      ${dancingSemi};
+      font-size: 28px;
+      line-height: 34px;
+    }
+    width: 290px;
+    .icons-social,
+    span {
+      ${displayFlex};
+      justify-content: center;
+    }
+  }
+`;
