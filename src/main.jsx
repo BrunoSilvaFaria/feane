f06bb0db5e0fdd7ac93f72a467ccf0eec6100b0d
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-22 10:57:33 -0300

    feat: adição de rotas para as páginas

--- src/main.jsx
+++ src/main.jsx
@@ -6,21 +6,31 @@ import {
 } from "react-router-dom";
 import App from './App';
 import Home from './pages/Home/Home';
-import Menu from './pages/Menu/Menu'
+import Menu from './pages/Menu/Menu';
+import About from "./pages/About/About";
+import Table from "./pages/Table/Table";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { 
+      {
         path: "/",
         element: <Home />,
       },
-      { 
+      {
         path: "/menu",
         element: <Menu />,
-      }
-    ]
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/table",
+        element: <Table />,
+      },
+    ],
   },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
