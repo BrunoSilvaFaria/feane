ae955101a4906c0b33979adbc248580cfc2f95d0
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-22 10:57:53 -0300

    feat: adição de footer, sendo reutilizado por todo o site

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/Header';
 import './global.css'
-// import Footer from './components/Footer/Footer';
+import Footer from './components/Footer/Footer';
 import ScrollToTop from './components/ScrollToTop';
 
 import { Outlet } from 'react-router-dom';
@@ -11,7 +11,7 @@ function App() {
 
       <Outlet />
       
-      {/* <Footer /> */}
+      <Footer />
       
       <ScrollToTop />
     </>
