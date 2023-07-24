c9ca2ae49791a8bd36d7191dcb5d049f5bb055f5
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-24 10:57:40 -0300

    feat: nova estrutura, sendo necessário refatoração do mesmo

--- src/helpers/tipografia.js
+++ src/helpers/tipografia.js
@@ -3,74 +3,34 @@ import { css } from "styled-components";
 
 const fonts = {
     dancing: "'Dancing Script', cursive",
-    fontRegular: 400,
-    fontSemiBold: 600,
-    fontBold: 700,
-}
-
-const openRegular = css`
-    font-weight: ${fonts.fontRegular};
-`;
-const openRegularBlack = css`
-  ${openRegular};
-  color: ${colors.black};
-`;
-const openBold= css`
-    ${openRegular};
-    font-weight: ${fonts.fontBold};
-`;
-
-const openBoldBlack = css`
-  ${openBold};
-  color: ${colors.black};
-`;
-const dancingBold = css`
-    font-family: ${fonts.dancing};
-    font-weight: ${fonts.fontBold};
-`
-const dancingBoldBlack = css`
-  ${dancingBold};
-  color: ${colors.black};
-`;
-
-const dancingSemi = css`
-    ${dancingBold};
-    font-weight: ${fonts.fontSemi};
-`
-const dancingSemiBlack = css`
-  ${dancingSemi};
-  color: ${colors.black};
-`;
+    weight: {
+        400: "400",
+        500: "500",
+        600: "600",
+        700: "700",
+    }
+};
 
 const bodyHover = css`
-    &:hover{
-        color: ${colors.yellowLight};
-    }
+  &:hover {
+    color: ${colors.yellowLight};
+  }
 `;
 
 const HeaderDancing = css`
-  ${dancingBold}
+  font-family: ${fonts.dancing};
   font-size: 2.5em;
   line-height: 1.2em;
-  margin-bottom: 25px;
 `;
 
 const ParagraphOpen = css`
-  ${openRegular};
   font-size: 1em;
   line-height: 1.5em;
 `;
+
 export {
   fonts,
-  dancingBold,
-  dancingBoldBlack,
-  dancingSemi,
-  dancingSemiBlack,
-  openBold,
-  openBoldBlack,
-  openRegular,
-  openRegularBlack,
   bodyHover,
   HeaderDancing,
-  ParagraphOpen
+  ParagraphOpen,
 };
