149e9bd6459bee20c4b176654401efae4d352ae0
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-24 10:57:18 -0300

    feat: adição dos componentes de promoção e relatos de clientes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,23 @@
 import Menu from '../Menu/Menu';
 import About from '../About/About';
 import Table from "../Table/Table";
+import { criaElementos } from '../../helpers/criaElementos';
+import  discount  from '../../data/discount';
+import Card from '../../components/Cards/Promocoes/Card';
 
+import Slide from '../../components/Slide/Slide';
+
+import { Discount } from './styles';
 function Home() {
   return (
     <div>
+      <Discount>
+        {criaElementos(discount, Card)}
+      </Discount>
       <Menu />
       <About />
       <Table />
+      <Slide />
     </div>
   );
 }
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+import { displayFlex } from "../../components/styles";
+export const Discount = styled.section`
+  ${displayFlex};
+  justify-content: center;
+  align-items: flex-end;
+  gap: 30px;
+  height: 300px;
+`;
