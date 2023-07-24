555857ad3e5ea898c8e52c202c98b9e3018160e7
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-24 10:56:22 -0300

    feat: estilos do footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,68 +13,69 @@ import {
 function Footer() {
   return (
     <Conteiner>
-      <div className="conteiners">
-        <div className="conteiner">
-          <h3>Contact Us </h3>
-          <span>
-            <IconLocation />
-            <a href="#">Location</a>
-          </span>
-          <span>
-            <IconPhone />
-            <a href="tel=+01 1234567890">Call +01 1234567890</a>
-          </span>
-          <span>
-            <IconEnvelope />
-            <a href="mailto:demo@gmail.com">demo@gmail.com</a>
-          </span>
+      <div className="alinhamento">
+        <div className="conteiners">
+          <div className="conteiner">
+            <h3>Contact Us </h3>
+            <span className='iconConteiner'>
+              <IconLocation  className='icon'/>
+              <a href="#">Location</a>
+            </span>
+            <span className='iconConteiner'>
+              <IconPhone className='icon'/>
+              <a href="tel=+01 1234567890">Call +01 1234567890</a>
+            </span>
+            <span className='iconConteiner'>
+              <IconEnvelope className='icon'/>
+              <a href="mailto:demo@gmail.com">demo@gmail.com</a>
+            </span>
+          </div>
+          <div className="conteiner">
+            <h2>Feane</h2>
+            <p>
+              Necessary,making this the first true generator on the Internet. It
+              uses a dictionary of over 200 Latin words, combined with
+            </p>
+            <nav>
+              <ul className='icons-social'>
+                <li>
+                  <Link>
+                    <IconFacebook className='icon'/>
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <IconTwitter className='icon'/>
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <IconLinkedin className='icon'/>
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <IconInstagram className='icon' />
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <IconPinterest className='icon' />
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="conteiner">
+            <h3>Opening Hours</h3>
+            <p>Everyday</p>
+            <p>10.00 Am -10.00 Pm</p>
+          </div>
         </div>
-        <div className="conteiner">
-          <h2>Feane</h2>
-          <p>
-            Necessary,making this the first true generator on the Internet. It
-            uses a dictionary of over 200 Latin words, combined with
-          </p>
-          <nav>
-            <ul className='icons-social'>
-              <li>
-                <Link>
-                  <IconFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <IconTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <IconLinkedin />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <IconInstagram />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <IconPinterest />
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-        <div className="conteiner">
-          <h3>Opening Hours</h3>
-          <p>Everyday</p>
-          <p>10.00 Am -10.00 Pm</p>
-        </div>
-      </div>
-
-      <div className="conteiner">
-        <p>© 2023 All Rights Reserved By Free Html Templates</p>
-        <p>© Distributed By ThemeWagon</p>
+        <footer >
+          <p>© 2023 All Rights Reserved By Free Html Templates</p>
+          <p>© Distributed By ThemeWagon</p>
+        </footer>
       </div>
     </Conteiner>
   );
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,17 +1,25 @@
 import { styled} from "styled-components";
 import { displayFlex } from '../styles';
 import { openRegular, dancingBold, dancingSemi } from "../../helpers/tipografia";
+import colors from "../../helpers/color";
 
 export const Conteiner = styled.header`
   background-color: #222831;
+  height: 456px;
   ${openRegular};
   font-size: 1em;
   line-height: 24px;
   text-align: center;
+  .alinhamento{
+    ${displayFlex};
+    flex-wrap: wrap;
+    height: inherit;
+  }
   .conteiners {
     width: 90%;
     max-width: 960px;
     margin: auto;
+    margin-bottom: 30px;
   }
   .conteiners,
   .icons-social {
@@ -20,6 +28,7 @@ export const Conteiner = styled.header`
   }
 
   .conteiner {
+    width: 290px;
     h3,
     h2 {
       margin-bottom: 25px;
@@ -34,11 +43,41 @@ export const Conteiner = styled.header`
       font-size: 28px;
       line-height: 34px;
     }
-    width: 290px;
+
+    .iconConteiner {
+      margin-bottom: 10px;
+      .icon {
+        margin-right: 8px;
+      }
+    }
+    p {
+      margin-bottom: 20px;
+    }
     .icons-social,
-    span {
+    span,
+    li a {
       ${displayFlex};
       justify-content: center;
+    }
+
+    li a {
+      background: white;
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+      margin-right: 5px;
+      .icon path {
+        color: #222831;
+        &:hover{
+          color: ${colors.yellowLight};
+        }
+      }
+    }
+  }
+  footer{
+    width: 100%;
+    p{
+      margin-bottom: 25px;
     }
   }
 `;
