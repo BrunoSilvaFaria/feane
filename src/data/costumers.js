dc730e2075c0720b5f4f3495846575f290ada3f1
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-24 10:55:12 -0300

    feat: adição de itens para promoção e relato de clientes

---
+++ src/data/costumers.js
@@ -0,0 +1,20 @@
+const costumers = [
+  {
+    id: 1,
+    name: "Moana Michell",
+    img: "/assets/client1.jpg",
+    function: "magna aliqua",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  },
+  {
+    id: 2,
+    name: "Mike Hamell ",
+    img: "/assets/client2.jpg",
+    function: "magna aliqua",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  },
+];
+
+export default costumers;
---
+++ src/data/discount.js
@@ -0,0 +1,16 @@
+const discount = [
+  {
+    id: 1,
+    name: "Tasty Thursdays ",
+    img: "/assets/o1.jpg",
+    discount: "20",
+  },
+  {
+    id: 2,
+    name: "Pizza Days ",
+    img: "/assets/o2.jpg",
+    discount: "15",
+  },
+];
+
+export default discount;
