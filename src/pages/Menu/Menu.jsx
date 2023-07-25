8cc9e39bd36cf0739c3152064010911bdaf19b8e
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-25 14:09:20 -0300

    feat: adição de cardápio responsivo

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -20,58 +20,60 @@ function Menu() {
     <Conteiner>
       <Header>
         <h3>Our Menu</h3>
-        <ul>
-          <li>
-            <a
-              className={activeLink === "All" ? "active" : ""}
-              onClick={() => {
-                handleLinkClick("All");
-              }}
-            >
-              All
-            </a>
-          </li>
-          <li>
-            <a
-              className={activeLink === "Burger" ? "active" : ""}
-              onClick={() => {
-                handleLinkClick("Burger");
-              }}
-            >
-              Burguer
-            </a>
-          </li>
-          <li>
-            <a
-              className={activeLink === "Pizza" ? "active" : ""}
-              onClick={() => {
-                handleLinkClick("Pizza");
-              }}
-            >
-              Pizza
-            </a>
-          </li>
-          <li>
-            <a
-              className={activeLink === "Pasta" ? "active" : ""}
-              onClick={() => {
-                handleLinkClick("Pasta");
-              }}
-            >
-              Pasta
-            </a>
-          </li>
-          <li>
-            <a
-              className={activeLink === "Fries" ? "active" : ""}
-              onClick={() => {
-                handleLinkClick("Fries");
-              }}
-            >
-              Fries
-            </a>
-          </li>
-        </ul>
+        <nav>
+          <ul>
+            <li>
+              <a
+                className={activeLink === "All" ? "active" : ""}
+                onClick={() => {
+                  handleLinkClick("All");
+                }}
+              >
+                All
+              </a>
+            </li>
+            <li>
+              <a
+                className={activeLink === "Burger" ? "active" : ""}
+                onClick={() => {
+                  handleLinkClick("Burger");
+                }}
+              >
+                Burguer
+              </a>
+            </li>
+            <li>
+              <a
+                className={activeLink === "Pizza" ? "active" : ""}
+                onClick={() => {
+                  handleLinkClick("Pizza");
+                }}
+              >
+                Pizza
+              </a>
+            </li>
+            <li>
+              <a
+                className={activeLink === "Pasta" ? "active" : ""}
+                onClick={() => {
+                  handleLinkClick("Pasta");
+                }}
+              >
+                Pasta
+              </a>
+            </li>
+            <li>
+              <a
+                className={activeLink === "Fries" ? "active" : ""}
+                onClick={() => {
+                  handleLinkClick("Fries");
+                }}
+              >
+                Fries
+              </a>
+            </li>
+          </ul>
+        </nav>
       </Header>
 
       <Cards >
--- src/pages/Menu/styles.js
+++ src/pages/Menu/styles.js
@@ -6,21 +6,39 @@ import colors from "../../helpers/color";
 export const Conteiner = styled.header`
   margin: auto;
   margin-bottom: 100px;
-  max-width: 920px;
+  max-width: 1145px;
+  width: 95%;
+  @media (max-width: 450px) {
+    width: 90%;
+  }
 `;
 export const Header = styled.header`
-  width: 410px;
   margin: auto auto 50px auto;
   h3 {
+    color: ${colors.blackFont};
     font-size: 40px;
     margin: 100px auto 50px auto;
     text-align: center;
   }
 
+  nav {
+    margin: auto;
+  }
   ul {
     ${displayFlex};
+    flex-wrap: wrap;
+    justify-content: center;
   }
-  a{
+
+  @media (max-width: 465px) {
+    & {
+      margin: auto auto 20px auto;
+    }
+    li {
+      margin-bottom: 30px;
+    }
+  }
+  a {
     color: ${colors.blackFont};
     padding: 7px 25px;
   }
@@ -33,14 +51,46 @@ export const Header = styled.header`
 `;
 
 export const Cards = styled.section`
-    max-width: 920px;
-  article{
+  max-width: 1445px;
+  .motion {
+    max-width: 360px;
+    height: 420px;
+    @media (max-width: 1190px) {
+      width: 290px;
+      height: 440px;
+    }
+    @media (max-width: 990px) {
+      width: 330px;
+    }
+    @media (max-width: 765px) {
+      width: 240px;
+      height: 460px;
+    }
+    @media (max-width: 575px) {
+      width: 100%;
+      height: 420px;
+      margin: auto;
+    }
+  }
+  article {
     ${displayFlex};
     justify-content: flex-start;
     flex-wrap: wrap;
-    width: 100%;
-    margin: auto;
     gap: 24px;
+    width: fit-content;
+    @media (max-width: 1190px) {
+      width: 950px;
+      margin: auto;
+    }
+    @media (max-width: 990px) {
+      width: 685px;
+    }
+    @media (max-width: 765px) {
+      width: 505px;
+    }
+    @media (max-width: 575px) {
+      width: 100%;
+    }
   }
 
   .btn {
