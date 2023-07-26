d3c5df40072fe4a7375eab68e996712dc34232a3
BrunoSilvaFaria <fariabruno443@gmail.com>
2023-07-26 12:13:07 -0300

    feat: adição de mais relatos

--- src/data/costumers.js
+++ src/data/costumers.js
@@ -15,6 +15,22 @@ const costumers = [
     discription:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
   },
+  {
+    id: 3,
+    name: "Moana Michell",
+    img: "/assets/client1.jpg",
+    function: "magna aliqua",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  },
+  {
+    id: 4,
+    name: "Mike Hamell ",
+    img: "/assets/client2.jpg",
+    function: "magna aliqua",
+    discription:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam",
+  },
 ];
 
 export default costumers;
